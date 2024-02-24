503ecbb405e7b0a092c16dd4a7b9b8c83854eded
renan <renan.nic28@gmail.com>
2024-02-24 18:48:43 -0300

    Documetação e Readme 24-02-2024

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# **API Sistema de Promissoria FBD**
 
-<h6>Tenho que começar a fazer o <strong>README</strong></h6>
+## Sobre o Projeto
+* **Instuição:** Universidade Federal Rural de Pernambuco - Unidade Acadêmica de Serra Talhada.
+
+* **Curso:** Bacharelado em Sistemas de Informação
+
+* **Cadeira:** Fundamentos de Banco de Dados
+
+* **Dorcente:** Prof. Héldon José Oliveira Albuquerque
+
+* **Descrição:** Trata-se de uma application programming interface (api) desenvolvido com o objetivo de ser um sistema de geração e controle de promissorias.
+
+## Link para Documentação 
+* [Link da Documentação em Swagger](http://localhost:8080/documentation/)
+
+*<p>Obs: Essa é uma api local então você deve rodar a aplicação primeiro para que  o link acima funcione corretamente.</p>*
+
+![Alt](image\image_swagger.png)
+
+## Autor
+* Renan Nicolau Gomes
--- lib/Modules/GeneralController.dart
+++ lib/Modules/GeneralController.dart
@@ -6,32 +6,32 @@ import 'package:sistema_promissorias/Modules/Cliente/controller.dart';
 import 'package:sistema_promissorias/Modules/Contrato/controller.dart';
 import 'package:sistema_promissorias/Modules/Parcela/controller.dart';
 import 'package:sistema_promissorias/Modules/Produto/controller.dart';
+import 'package:sistema_promissorias/Modules/swagger/swagger_handler.dart';
 
 import 'Item_Produto/controller.dart';
+
 /// Gerenciamento geral de rotas
 class GeneralController {
-    Handler get handler{
-      final route = Router();
+  Handler get handler {
+    final route = Router();
 
-      // todas as rotas de cliente
-      route.mount('/cliente', ClienteHandlerController().router);
+    // todas as rotas de cliente
+    route.mount('/cliente', ClienteHandlerController().router);
 
-      // todas as rotas de produto
-      route.mount('/produto', ProdutoControllerHandler().router);
+    // todas as rotas de produto
+    route.mount('/produto', ProdutoControllerHandler().router);
 
-      // todas as rotas de contrato
-      route.mount('/contrato', ContratoHandlerController().router);
+    // todas as rotas de contrato
+    route.mount('/contrato', ContratoHandlerController().router);
 
-      // todas as rotas de parcelas
-      route.mount('/parcela', ParcelaHandlerController().router);
+    // todas as rotas de parcelas
+    route.mount('/parcela', ParcelaHandlerController().router);
 
-      // todas as rotas de item_produto
-      route.mount('/item_produto', ItemProdutoController().router);
+    // todas as rotas de item_produto
+    route.mount('/item_produto', ItemProdutoController().router);
 
-      return route;
+    route.get('/documentation/<name|.*>', swaggerHandler);
 
-
-    }
-
-
+    return route;
+  }
 }
